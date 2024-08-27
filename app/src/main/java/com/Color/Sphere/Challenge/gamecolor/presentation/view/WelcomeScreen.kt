099b3cfb47d07c15e421625bbf7713f6225f5d2d
tanknatan <tanknatanrezet@gmail.com>
2024-08-27 12:20:36 +0300

    https://t.me/ye_nfrjt Color Sphere Challenge

--- app/src/main/java/com/Color/Sphere/Challenge/gamecolor/presentation/view/WelcomeScreen.kt
+++ app/src/main/java/com/Color/Sphere/Challenge/gamecolor/presentation/view/WelcomeScreen.kt
@@ -1,11 +1,8 @@
 package com.Color.Sphere.Challenge.gamecolor.presentation.view
 
+
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.runtime.Composable
-
-
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,23 +12,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.Color.Sphere.Challenge.gamecolor.R
 
 
